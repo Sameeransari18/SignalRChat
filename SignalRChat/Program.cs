e1cf5f09f01b80d648ca009316c7223067667b2d
Dell <Dell@ZSS008>
2023-01-09 15:20:47 +0530

    Add project files.

---
+++ SignalRChat/Program.cs
@@ -0,0 +1,35 @@
+// -- Adding SignalR configuration to request SignalR
+using SignalRChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// -- Adding the SignalR
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+// -- Adding the DI SignalR
+app.MapHub<ChatHub>("/ChatHub");
+
+
+app.Run();
